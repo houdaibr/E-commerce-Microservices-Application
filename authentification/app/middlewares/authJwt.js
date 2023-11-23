4bd956b82149de673b0dafea8a19ab4dc690ecc5
Houda EL IBRAHIMI (Intern) <H.IBRAHIMI.Intern@netopia.ma>
2023-11-23 10:31:34 +0100

    adding authentification service

---
+++ authentification/app/middlewares/authJwt.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+
+
+verifyToken = (req, res, next) => {
+const token = req.headers && req.headers["authorization"].split(' ')[1]
+
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+
+  jwt.verify(token,
+            config.secret,
+            (err, decoded) => {
+              if (err) {
+                return res.status(401).send({
+                  message: "Unauthorized!",
+                });
+              }
+              req.userId = decoded.id;
+              next();
+            });
+};
+
+
+
+
+
+const authJwt = {
+  verifyToken
+ 
+};
+module.exports = authJwt;
---
+++ authentification/app/routes/auth.routes.js
@@ -0,0 +1,21 @@
+// auth.routes.js
+const { verifySignUp } = require("../middlewares");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept, Authorization" // Include "Authorization" header
+    );
+    next();
+  });
+
+  app.post(
+    "/api/auth/signup",
+    [verifySignUp.checkDuplicateUsernameOrEmail],
+    controller.signup
+  );
+
+  app.post("/api/auth/signin", controller.signin);
+};
